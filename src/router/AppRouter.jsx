dfa668fbe9d179c6188f3a599cd0100c32760af4
Santiago Sosa <santisosalp@gmail.com>
2024-11-19 12:09:55 -0300

    problemas con login y persistencia de user

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,39 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 import {
   Route,
   Routes,
 } from 'react-router';
 
 import { AuthRoute } from '../auth/router/AuthRoute';
+import { journalAuth } from '../firebase';
 import { JournalRoute } from '../journal/router/Journalroutes';
+import {
+  loginEmail,
+  logout,
+} from '../store/auth';
 import { CheckingAuth } from '../ui';
 
 export const AppRouter = () => {
     const { status } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        onAuthStateChanged(journalAuth, async (user) => {
+            console.log(user)
+            if (!user) return dispatch(logout())
+            dispatch(loginEmail(user))
+        })
+    }, [])
 
     if (status === 'checking') return (
-    <CheckingAuth/>
+        <CheckingAuth />
     )
-
 
     return (
         <Routes>
